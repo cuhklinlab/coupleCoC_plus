6f22c073398e1bfd75360484ec4c785c701b4e44
pengchengzeng <55469380+pengchengzeng@users.noreply.github.com>
2021-04-15 10:40:29 +0800

    Update heatmap.R

--- R/heatmap.R
+++ R/heatmap.R
@@ -1,33 +1,33 @@
 # plot UMAP for the raw data
-plot.umap = function(x, labels, main, colors, pos = "topright", pad=0.1, cex=0.6, pch=19, add=FALSE, legend.suffix="", cex.main=1, cex.legend=0.85) {
-  layout = x
-  if (is(x, "umap")) {
-    layout = x$layout
-  }
+plot.umap = function(x, labels, main, colors, pos = "topright", pad=0.1, cex=1, pch=19, add=FALSE, legend.suffix="", cex.main=1.5, cex.legend=1.5) {
+   layout = x
+   if (is(x, "umap")) {
+     layout = x$layout
+   }
 
-  xylim = range(layout)
-  xylim = xylim + ((xylim[2]-xylim[1])*pad)*c(-0.5, 0.5)
+   xylim = range(layout)
+   xylim = xylim + ((xylim[2]-xylim[1])*pad)*c(-0.5, 0.5)
   if (!add) {
-    par(mar=c(0.2,0.7,1.2,0.7), ps=10)
-    plot(xylim, xylim, type="n", axes=F, frame=F, xlab = "UMAP1")
-    rect(xylim[1], xylim[1], xylim[2], xylim[2], border="#aaaaaa", lwd=0.25)
-  }
-  points(layout[,1], layout[,2], col=colors[as.integer(labels)],
-         cex=cex, pch=pch)
-  mtext(side=3, main, cex=cex.main)
+     par(mar=c(0.2,0.7,1.2,0.7), ps=10)
+     plot(xylim, xylim, type="n", axes=F, frame=F, xlab = "UMAP1")
+     rect(xylim[1], xylim[1], xylim[2], xylim[2], border="#aaaaaa", lwd=0.25)
+   }
+   points(layout[,1], layout[,2], col=colors[as.integer(labels)],
+          cex=cex, pch=pch)
+   mtext(side=3, main, cex=cex.main)
 
 
-  labels.u = unique(labels)
-  legend.pos = pos
-  legend.text = as.character(labels.u)
-  if (add) {
-    legend.pos = "bottomleft"
-    legend.text = paste(as.character(labels.u), legend.suffix)
-  }
+   labels.u = unique(labels)
+   legend.pos = pos
+   legend.text = as.character(labels.u)
+   if (add) {
+     legend.pos = "bottomleft"
+     legend.text = paste(as.character(labels.u), legend.suffix)
+   }
 
-  legend(legend.pos, legend=legend.text, inset=0.03,
-         col=colors[as.integer(labels.u)],
-         bty="n", pch=pch, cex=cex.legend)
+   legend(legend.pos, legend=legend.text, inset=0.03,
+          col=colors[as.integer(labels.u)],
+          bty="n", pch=pch, cex=cex.legend)
 }
 
 # functions for plotting the heatmaps
